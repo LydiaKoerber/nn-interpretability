07feae1edb2bde2d38dfc13bc4c8be7cef9178a9
LydiaKoerber <lydi.koerber@web.de>
2023-09-09 15:36:29 +0200

    process data in batches of 500

--- captum-explain.py
+++ captum-explain.py
@@ -20,22 +20,25 @@ def setup_distilbert():
                         tokenizer = tok_dist)
     return clf_dist
 
-def explain_all(test_data, exp_model):
+def explain_all(test_data, exp_model, subsplit_size=500):
     df = pd.DataFrame(columns=['tokens', 'attributions'])
-    for i, d in enumerate(test_data):
-        try:
-            a, pred = exp_model.explain(d['text'])
-            new_row = pd.DataFrame({
-                'label_pred': pred[0]['label'],
-                'score': pred[0]['score'],
-                'tokens': [a.index.tolist()],
-                'attributions': [a.tolist()]
-            })
-            df = df.append(new_row, ignore_index=True)
-        except:
-            print(i)
-            df = df.append([None, None, None, None], ignore_index=True)
-    df.to_csv(f'outputs/{exp_model.model}_attributions.csv')
+    for j in range(0, len(test_data), subsplit_size):
+        print(f'split {j}')
+        subsplit = test_data[j:j + subsplit_size]
+        for i, d in enumerate(subsplit):
+            try:
+                a, pred = exp_model.explain(d['text'])
+                new_row = pd.DataFrame({
+                    'label_pred': pred[0]['label'],
+                    'score': pred[0]['score'],
+                    'tokens': [a.index.tolist()],
+                    'attributions': [a.tolist()]
+                })
+                df = df.append(new_row, ignore_index=True)
+            except:
+                print(i)
+                df = df.append([None, None, None, None], ignore_index=True)
+        df.to_csv(f'outputs/{exp_model.model}_attributions_{j}.csv')
 
 if __name__ == '__main__':
     # data["test"][0]
